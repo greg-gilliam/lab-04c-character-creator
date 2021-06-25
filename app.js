d62903ce6be41985b866b67254c4fc84a8b7abef
Triana Cerda <trianaRcerda@gmail.com>
2021-06-24 17:27:45 -0700

    added babylion1

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-//var strength = 1;
-//if(strength === 1){
-  //console.log(strength);
-//}
-//console.log(strength);
 
+const babyLion1 = document.getElementById('baby-lion-1');
 const submitButton = document.getElementById('results');
 submitButton.addEventListener('click', ()=>{
-
+    babyLion1.style.display = 'block';
 });
+
+function displayImage(){
+    var strength = 1;
+    if(strength === 1){
+        console.log(strength);
+}
+
 
